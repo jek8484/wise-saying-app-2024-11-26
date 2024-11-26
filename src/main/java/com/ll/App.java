8bfb2b1ae218494f303305412fe70883835e93bf
jek8484 <ysms3611@gmail.com>
2024-11-26 14:33:59 +0900

    work

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,17 @@
+package com.ll;
+
+import java.util.Scanner;
+
+public class App {
+    private final Scanner scanner;
+
+    public App(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+
+        System.out.print("명령) ");
+    }
+}
---
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -0,0 +1,31 @@
+package com.ll.domain.wiseSaying.controller;
+
+import com.ll.AppTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class WiseSayingControllerTest {
+    @Test
+    @DisplayName("== 명언 앱 ==")
+    public void t1() {
+        String output = AppTest.run("종료");
+
+        assertThat(output)
+                .contains("== 명언 앱 ==");
+    }
+
+    @Test
+    @DisplayName("명령) ")
+    public void t2() {
+        String output = AppTest.run("""
+                목록
+                종료
+                """);
+
+        assertThat(output)
+                .contains("명령) ");
+    }
+}
