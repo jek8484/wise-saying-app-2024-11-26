f6da6ad483d8205a7fdcaadcbe97a578e1cbaa24
jek8484 <ysms3611@gmail.com>
2024-11-26 15:59:02 +0900

    work

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -21,4 +21,62 @@ public class WiseSayingControllerTest {
                 .contains("명언 : ")
                 .contains("작가 : ");
     }
+
+    @Test
+    @DisplayName("등록이 완료되면 명언번호가 출력된다.")
+    public void t5() {
+        String output = AppTest.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(output)
+                .contains("1번 명언이 등록되었습니다.");
+    }
+
+    @Test
+    @DisplayName("매번 생성되는 명언번호는 1씩 증가 한다.")
+    public void t6() {
+        String output = AppTest.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(output)
+                .contains("1번 명언이 등록되었습니다.")
+                .contains("2번 명언이 등록되었습니다.")
+                .contains("3번 명언이 등록되었습니다.");
+    }
+
+    @Test
+    @DisplayName("목록 명령어 : 입력된 명언들을 출력한다.")
+    public void t7() {
+        String output = AppTest.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                등록
+                나의 죽음을 적들에게 알리지 말라!
+                이순신
+                목록
+                """);
+
+        assertThat(output)
+                .contains("번호 / 작가 / 명언")
+                .contains("----------------------")
+                .contains("3 / 이순신 / 나의 죽음을 적들에게 알리지 말라!")
+                .contains("2 / 작자미상 / 과거에 집착하지 마라.")
+                .contains("1 / 작자미상 / 현재를 사랑하라.");
+    }
 }
