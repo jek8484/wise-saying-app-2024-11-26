f8ce21c1e5e39c686d9e5e2661de45cc994c5581
jek8484 <ysms3611@gmail.com>
2024-11-27 16:07:20 +0900

    work

---
+++ src/main/java/com/ll/domain/wiseSaying/repository/WiseSayingFileRepository.java
@@ -0,0 +1,48 @@
+package com.ll.domain.wiseSaying.repository;
+
+import com.ll.domain.wiseSaying.entity.WiseSaying;
+import com.ll.standard.util.Util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class WiseSayingFileRepository implements WiseSayingRepository {
+    private final List<WiseSaying> wiseSayings;
+    private int lastId;
+
+    public WiseSayingFileRepository() {
+        this.wiseSayings = new ArrayList<>();
+        this.lastId = 0;
+    }
+
+    public WiseSaying save(WiseSaying wiseSaying) {
+        if (!wiseSaying.isNew()) {
+            return wiseSaying;
+        }
+
+        wiseSaying.setId(++lastId);
+
+        Map<String, Object> wiseSayingMap = wiseSaying.toMap();
+        String jsonStr = Util.json.toString(wiseSayingMap);
+
+        Util.file.set("db/test/wiseSaying/1.json", jsonStr);
+
+        return wiseSaying;
+    }
+
+    public List<WiseSaying> findAll() {
+        return wiseSayings;
+    }
+
+    public boolean deleteById(int id) {
+        return wiseSayings.removeIf(wiseSaying -> wiseSaying.getId() == id);
+    }
+
+    public Optional<WiseSaying> findById(int id) {
+        return wiseSayings.stream()
+                .filter(wiseSaying -> wiseSaying.getId() == id)
+                .findFirst();
+    }
+}
