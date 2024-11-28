e8d94d2860572582485197d9edf03fb3a78b0121
jek8484 <ysms3611@gmail.com>
2024-11-28 10:29:29 +0900

    work

--- src/test/java/com/ll/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
+++ src/test/java/com/ll/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
@@ -2,36 +2,59 @@ package com.ll.domain.wiseSaying.repository;
 
 import com.ll.domain.wiseSaying.entity.WiseSaying;
 import com.ll.standard.util.Util;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WiseSayingFileRepositoryTest {
     private final WiseSayingRepository wiseSayingRepository = new WiseSayingFileRepository();
 
-    @BeforeAll
-    public static void beforeAll() {
-        Util.file.rmdir("db");
-        Util.file.mkdir("db");
+    @BeforeEach
+    public void beforeEach() {
+        Util.file.rmdir(WiseSayingFileRepository.getTableDirPath());
     }
 
-    @AfterAll
-    public static void afterAll() {
-        Util.file.rmdir("db");
+    @AfterEach
+    public void afterEach() {
+        Util.file.rmdir(WiseSayingFileRepository.getTableDirPath());
     }
 
     @Test
     @DisplayName("명언 저장")
     public void t1() {
-        wiseSayingRepository.save(new WiseSaying(0, "명언1", "저자1"));
+        WiseSaying wiseSaying = new WiseSaying(0, "꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "괴테");
+        wiseSayingRepository.save(wiseSaying);
 
-        String filePath = "db/test/wiseSaying/1.json";
+        String filePath = WiseSayingFileRepository.getRowFilePath(wiseSaying.getId());
 
         assertThat(
                 Util.file.exists(filePath)
         ).isTrue();
+
+        String jsonStr = Util.file.get(filePath, "");
+        Map<String, Object> wiseSayingMap = Util.json.toMap(jsonStr);
+        WiseSaying wiseSayingRestored = new WiseSaying(wiseSayingMap);
+
+        assertThat(wiseSayingRestored).isEqualTo(wiseSaying);
+    }
+
+    @Test
+    @DisplayName("명언 삭제")
+    public void t2() {
+        WiseSaying wiseSaying = new WiseSaying(0, "꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "괴테");
+        wiseSayingRepository.save(wiseSaying);
+
+        wiseSayingRepository.deleteById(wiseSaying.getId());
+
+        String filePath = WiseSayingFileRepository.getRowFilePath(wiseSaying.getId());
+
+        assertThat(
+                Util.file.exists(filePath)
+        ).isFalse();
     }
 }
